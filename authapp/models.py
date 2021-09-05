372b01ea60c8d0fee8a33a1b809ad0df91f01b93
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-09-05 18:24:13 +0500

    step3

--- authapp/models.py
+++ authapp/models.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 import re
 
 
-# class PhoneValidator():
-#     phone_pattern = re.compile(r"(?:\+7|8)(?:-\d{2,3}){4}")
+# class PhoneValidator(re.compile(r"(?:\+7|8)(?:-\d{2,3}){4}"))
 
 
 
@@ -18,7 +18,11 @@ class UserProfile(AbstractUser):
         (GENDER_FEMALE, _('female')),
     )
 
+#    phone_pattern = RegexValidator(regex=r"(?:\+7|8)(\d{2,3}){4}")
     date_birth = models.DateField(_('birth date'), null=True)
-    # phone_number = models.CharField(validators=[PhoneValidator])
+    phone_number = models.CharField #(blank=True, max_length=12, validators=[phone_pattern])
     gender = models.CharField(_('gender'), max_length=1,
                               choices=GENDER_CHOICES, blank=True)
+
+    def __str__(self):
+        return f'{self.username}'
