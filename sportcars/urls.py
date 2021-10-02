011167df9e349ee29b669e861eef3886e38d8e36
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-10-02 15:27:42 +0500

    210920

--- sportcars/urls.py
+++ sportcars/urls.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+import authapp.views as authapp
+import mainapp.views as mainapp
+
+router = DefaultRouter()
+router.register('users', authapp.UserViewSet)
+router.register('projects', mainapp.ProjectViewSet)
+router.register('project-tasks', mainapp.ProjectTaskViewSet)
 
 urlpatterns = [
+    path('', mainapp.ProjectList.as_view()),
+    path('project/tasks/', mainapp.ProjectTaskList.as_view()),
+
+    path('api/', include(router.urls)),
+
     path('admin/', admin.site.urls),
 ]
