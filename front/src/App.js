5820dbf0ae79c2a5a3af555cf7997e34d599d3be
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-10-04 10:25:10 +0500

    Menu in new component

--- front/src/App.js
+++ front/src/App.js
@@ -252,34 +252,6 @@ class App extends React.Component {
                 <Router>
                     <Header/>
 
-                    <header>
-                        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                            <div className="container-fluid">
-                                <Link to={"/"}>Sportcars</Link>
-                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false"
-                                        aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <div className="collapse navbar-collapse" id="navbarText">
-                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                        <li className="nav-item">
-                                            <div className="nav-link active" aria-current="page"><Link
-                                                to={"/cars"}>Cars</Link></div>
-                                        </li>
-                                        <div className="nav-item">
-                                            <div className="nav-link"><Link to={"/users"}>Users</Link></div>
-                                        </div>
-                                    </ul>
-                                    <span className="navbar-text">
-                        </span>
-                                </div>
-                            </div>
-                        </nav>
-
-
-                    </header>
-
                     <Route exact path="/users">
                         <UserList users={this.state.users}/>
                     </Route>
